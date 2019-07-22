52ccd76b405b05a2471930ae144105fc0bf78d5e
qidasheng2012 <qidasheng2012@163.com>
2019-07-22 15:58:33 +0800

    删除多余注解

--- src/main/java/com/springboot/ssm/mapper/UserMapper.java
+++ src/main/java/com/springboot/ssm/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.springboot.ssm.mapper;
 
 import com.springboot.ssm.domain.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,7 +10,6 @@ import java.util.List;
  * @date 2019/7/21 17:00
  */
 @Mapper
-@Repository
 public interface UserMapper {
     // 查询所有学生信息
     List<User> getAll();
